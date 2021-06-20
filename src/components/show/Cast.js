e52519d23ab75cd0afc566248fe599d6da95fb0f
Alice <59229770+1myourman@users.noreply.github.com>
2021-06-20 16:05:25 +0900

    styled show page

---
+++ src/components/show/Cast.js
@@ -0,0 +1,30 @@
+import React from 'react';
+// eslint-disable-next-line import/no-unresolved
+import IMG_PLACEHOLDER from '../../misc/Asstes - BoxOffice/not_found.png';
+import { CastList } from './Cast.styled';
+
+const Cast = ({ cast }) => {
+  return (
+    <CastList>
+      {cast.map(({ person, character, voice }, key) => (
+        <div key={key} className="cast-item">
+          <div className="pic-wrapper">
+            <img
+              src={person.image ? person.image.medium : IMG_PLACEHOLDER}
+              alt="cast-person"
+            />
+          </div>
+          <div className="actor">
+            <span>
+              <span className="bold">
+                {person.name} | {character.name} {voice ? '| Voice' : ''}
+              </span>
+            </span>
+          </div>
+        </div>
+      ))}
+    </CastList>
+  );
+};
+
+export default Cast;
